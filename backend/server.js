f560821231654e9c1246c8a65221d725b316675b
ayushsvt16 <ayush.230103011@iiitbh.ac.in>
2026-02-19 12:55:11 +0530

    Fixed production API URL interpolation

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://codewithsaathi.vercel.app"],
+    origin: "*",
     methods: ["GET", "POST", "PUT"]
   }
 });
