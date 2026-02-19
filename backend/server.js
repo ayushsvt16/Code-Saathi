c07113c4cf72708e766f03b74d96a33813f4abee
ayushsvt16 <ayush.230103011@iiitbh.ac.in>
2026-02-19 12:23:24 +0530

    Render configurations

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: ["http://localhost:5173", "https://codewithsaathi.vercel.app"],
     methods: ["GET", "POST", "PUT"]
   }
 });
